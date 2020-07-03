8cf32ba649142d9df6968ca736208532dea99174
SamHuang <diwang90@gmail.com>
2020-07-03 15:34:28 +0800

    406 queue reconstruction by height     queue reconstruction by height406 queue reconstruction by height

--- greedy/406_queue_reconstruction_by_height/main.cpp
+++ greedy/406_queue_reconstruction_by_height/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+using namespace std;
 
 class Solution {
   public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>> result;
+        sort(people.begin(), people.end(), [](vector<int> &a, vector<int> &b) {
+              if (a[0] > b[0]) {
+                return true;
+              } else {
+                if (a[0] < b[0]){
+                  return false;
+                } else {
+                  return a[1] < b[1];
+                }
+              }
+            });   
+        for(auto &i: people) {
+            result.insert(result.begin()+i[1], i);
+        }
+        return result;
+    }
+};
 
+
+int main() {
+  vector<vector<int>> people{{7,0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+  auto solution = Solution();
+  auto result = solution.reconstructQueue(people);
+  for(auto &i: result) {
+    cout<<i[0]<<i[1]<<endl;
+  }
+  return 0;
 }
+
